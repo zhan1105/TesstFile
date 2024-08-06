747bd79ae6ae0dee64744a10b175f8ff7e843fe0
Johnny <zhan0619@gmail.com>
2024-08-06 20:07:31 +0800

    Initial Commit

---
+++ TesstFile/TesstFileApp.swift
@@ -0,0 +1,17 @@
+//
+//  TesstFileApp.swift
+//  TesstFile
+//
+//  Created by 紹郁 on 2024/8/6.
+//
+
+import SwiftUI
+
+@main
+struct TesstFileApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
